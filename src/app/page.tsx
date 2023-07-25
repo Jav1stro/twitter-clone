514d4d170b9a42f0e390856dc5afe265809368bc
Javier Amarilla <javier.amarilla@casademoneda.gob.ar>
2023-07-25 11:57:25 -0300

    Componente LeftSidebar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,57 +1,11 @@
-import Link from "next/link";
-import { BiHomeCircle, BiUser } from "react-icons/bi";
-import { BsBell, BsBookmark, BsTwitter } from "react-icons/bs";
-import { HiOutlineHashtag } from "react-icons/hi";
-import { HiEnvelope } from "react-icons/hi2";
+import LeftSideBar from "../components/LeftSidebar.jsx";
 
-const NAVIGATION_ITEMS = [
-  {
-    title: "Home",
-    icon: BiHomeCircle,
-  },
-  {
-    title: "Explore",
-    icon: HiOutlineHashtag,
-  },
-  {
-    title: "Notifications",
-    icon: BsBell,
-  },
-  {
-    title: "Message",
-    icon: HiEnvelope,
-  },
-  {
-    title: "BookMark",
-    icon: BsBookmark,
-  },
-  {
-    title: "Profile",
-    icon: BiUser,
-  },
-];
 const Home = () => {
   return (
     <div className="w-full h-full flex justify-center items-center relative bg-black">
-      <div className="max-w-screen-lg w-full h-full flex relative">
+      <div className="max-w-screen-xl w-full h-full flex relative">
         {/* Left sidebar for navigation */}
-        <section className="fixed w-72 flex flex-col">
-          <Link href={"/"} className="my-4">
-            <BsTwitter />
-          </Link>
-          {NAVIGATION_ITEMS.map((item) => (
-            <Link
-              href={`/${item.title.toLowerCase()}`}
-              key={item.title}
-              className="hover:bg-white/10 transition duration-200 rounded-3xl p-2 flex items-center justify-start w-fit space-x-2"
-            >
-              <div>
-                <item.icon />
-              </div>
-              <div>{item.title}</div>
-            </Link>
-          ))}
-        </section>
+        <LeftSideBar />
         {/* <main>Home</main>
         <section>Right section</section> */}
       </div>
---
+++ src/components/LeftSidebar.jsx
@@ -0,0 +1,82 @@
+import Link from "next/link";
+import { BiHomeCircle, BiUser } from "react-icons/bi";
+import {
+  BsBell,
+  BsBookmark,
+  BsTwitter,
+  BsEnvelope,
+  BsThreeDots,
+} from "react-icons/bs";
+import { HiOutlineHashtag } from "react-icons/hi";
+
+
+const NAVIGATION_ITEMS = [
+  {
+    title: "Twitter",
+    icon: BsTwitter,
+  },
+  {
+    title: "Home",
+    icon: BiHomeCircle,
+  },
+  {
+    title: "Explore",
+    icon: HiOutlineHashtag,
+  },
+  {
+    title: "Notifications",
+    icon: BsBell,
+  },
+  {
+    title: "Message",
+    icon: BsEnvelope,
+  },
+  {
+    title: "BookMark",
+    icon: BsBookmark,
+  },
+  {
+    title: "Profile",
+    icon: BiUser,
+  },
+];
+
+const LeftSidebar = () => {
+  return (
+    <section className="fixed w-[275px] flex flex-col items-stretch  h-screen">
+      <div className="w-full flex flex-col items-stretch h-full space-y-4 mt-4">
+        {NAVIGATION_ITEMS.map((item) => (
+          <Link
+            href={`/${item.title.toLowerCase()}`}
+            key={item.title}
+            className="hover:bg-white/10 text-2xl transition duration-200 rounded-3xl py-2 px-6 flex items-center justify-start w-fit space-x-4"
+          >
+            <div>
+              <item.icon />
+            </div>
+            {item.title !== "Twitter" && <div>{item.title}</div>}
+          </Link>
+        ))}
+        <button className="rounded-full m-4 bg-primary p-4 text-2xl text-center hover:bg-opacity-70 transition duration-200">
+          Tweet
+        </button>
+      </div>
+
+      {/* bottom */}
+      <button className="rounded-full flex items-center space-x-2 m-4 bg-transparent p-4 text-center hover:bg-white/20 transition duration-200  justify-around">
+        <div className="flex gap-4 items-center">
+          <div className="rounded-full bg-slate-400 w-10 h-10">f</div>
+          <div className="text-left ">
+            <div className="font-semibold text-sm">UserName</div>
+            <div className="text-xs">@username</div>
+          </div>
+        </div>
+        <div>
+          <BsThreeDots />
+        </div>
+      </button>
+    </section>
+  );
+};
+
+export default LeftSidebar
